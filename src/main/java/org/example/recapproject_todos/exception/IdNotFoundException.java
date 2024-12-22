1f6c9a3a0a7b516d6e30207db8765896125a2c43
Torben Jost <torbenjost@proton.me>
2024-12-22 15:47:10 +0100

    implement exception handling

---
+++ src/main/java/org/example/recapproject_todos/exception/IdNotFoundException.java
@@ -0,0 +1,7 @@
+package org.example.recapproject_todos.exception;
+
+public class IdNotFoundException extends Exception {
+    public IdNotFoundException(String message) {
+        super(message);
+    }
+}
